79c690c74488ad4d4295b43c0633c8c0df1ef0c6
sheraz61 <hsheraz271@gmail.com>
2026-04-14 12:25:03 +0500

    Add WithdrawMoney component and ShopWithdrawMoneyPage for managing withdrawal processes

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -39,13 +39,13 @@ const ShopInfo = ({ isOwner }) => {
       });
   };
 
-  //   const totalReviewsLength =
-  //     products &&
-  //     products.reduce((acc, product) => acc + product.reviews.length, 0);
+    const totalReviewsLength =
+      products &&
+      products.reduce((acc, product) => acc + product.reviews.length, 0);
 
-  //   const totalRatings = products && products.reduce((acc,product) => acc + product.reviews.reduce((sum,review) => sum + review.rating, 0),0);
+    const totalRatings = products && products.reduce((acc,product) => acc + product.reviews.reduce((sum,review) => sum + review.rating, 0),0);
 
-  //   const averageRating = totalRatings / totalReviewsLength || 0;
+    const averageRating = totalRatings / totalReviewsLength || 0;
 
   return (
     <>
@@ -81,7 +81,7 @@ const ShopInfo = ({ isOwner }) => {
       </div>
       <div className="p-3">
         <h5 className="font-[600]">Shop Ratings</h5>
-        <h4 className="text-[#000000b0]">4.5/5</h4>
+        <h4 className="text-[#000000b0]">{averageRating}/5</h4>
       </div>
       <div className="p-3">
         <h5 className="font-[600]">Joined On</h5>
